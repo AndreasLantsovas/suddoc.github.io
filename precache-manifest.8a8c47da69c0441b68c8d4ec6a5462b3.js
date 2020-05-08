8176b9b88db54b63e1f1372a69baeda57160daa5
Andrey <healbedesign@MacBook.local>
2020-05-08 14:50:09 +0300

    add react app

---
+++ precache-manifest.8a8c47da69c0441b68c8d4ec6a5462b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb13b440f831021f7dad62fcd8e0dec",
+    "url": "/suddoc.github.io/index.html"
+  },
+  {
+    "revision": "2af0f5228128e249bdc3",
+    "url": "/suddoc.github.io/static/css/main.52654c73.chunk.css"
+  },
+  {
+    "revision": "a9d577d7513228655135",
+    "url": "/suddoc.github.io/static/js/2.913dfafd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/suddoc.github.io/static/js/2.913dfafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af0f5228128e249bdc3",
+    "url": "/suddoc.github.io/static/js/main.9eb8b88a.chunk.js"
+  },
+  {
+    "revision": "0d52cf5b3c1dd59f8ecd",
+    "url": "/suddoc.github.io/static/js/runtime-main.14c0c730.js"
+  }
+]);
